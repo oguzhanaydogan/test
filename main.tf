bfece91355960696d35f78a769cd3b5bb76cddff
Oguzhan AYDOGAN <oguzhan.aydogan@deop.ca>
2023-06-07 17:18:26 -0400

    small bugs

--- main.tf
+++ main.tf
@@ -103,7 +103,7 @@ resource "azurerm_service_plan" "example" {
 resource "azurerm_linux_web_app" "webapp1" {
   name                = "oaydoganwebapp"
   resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_service_plan.rg.location
+  location            = azurerm_resource_group.rg.location
   service_plan_id     = azurerm_service_plan.example.id
 
   site_config {}
@@ -112,7 +112,7 @@ resource "azurerm_linux_web_app" "webapp1" {
 resource "azurerm_linux_web_app" "webapp2" {
   name                = "oaydoganwebapp2"
   resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_service_plan.rg.location
+  location            = azurerm_resource_group.rg.location
   service_plan_id     = azurerm_service_plan.example.id
 
   site_config {}
