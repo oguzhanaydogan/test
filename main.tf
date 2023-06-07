f72c12e9d32e17aab01e60deb104bdb3863c2b6a
Oguzhan AYDOGAN <oguzhan.aydogan@deop.ca>
2023-06-07 16:58:44 -0400

    small bugs

--- main.tf
+++ main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "3.59.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "DefaultResourceGroup-EUS"
+    storage_account_name = "ccseyhan2"
+    container_name       = "terraformstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
