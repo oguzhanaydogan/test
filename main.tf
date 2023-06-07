43c14f4a678095940cfff7a9ce2b38ba543560ed
Oguzhan AYDOGAN <oguzhan.aydogan@deop.ca>
2023-06-07 18:05:28 -0400

    vm added

--- main.tf
+++ main.tf
@@ -288,3 +288,107 @@ resource "azurerm_private_endpoint" "acr_private_endpoint" {
     azurerm_container_registry.acr
   ]
 }
+
+resource "azurerm_virtual_machine" "vm1" {
+  name                  = var.vm_name
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_D2s_v3"
+  delete_os_disk_on_termination = true
+  delete_data_disks_on_termination = true
+  identity {
+    type = "SystemAssigned"
+  }
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = var.vm_name
+    admin_username = var.admin_username
+    custom_data = data.template_file.userdata.rendered
+  }
+  os_profile_linux_config {
+    disable_password_authentication = true
+    ssh_keys {
+      path = "/home/${var.admin_username}/.ssh/authorized_keys"
+      key_data = data.azurerm_ssh_public_key.ssh_public_key.public_key
+
+   }
+ }
+}
+
+resource "azurerm_public_ip" "pip1" {
+  name                = "${var.vm_name}-pip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.vm_name}-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = module.acr.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.pip1.id
+  }
+}
+
+
+data "azurerm_ssh_public_key" "ssh_public_key" {
+  resource_group_name = var.ssh_key_rg
+  name                = var.ssh_key_name
+}
+
+data "template_file" "userdata" {
+  template = file("${abspath(path.module)}/userdata.sh")
+}
+
+resource "azurerm_network_interface_security_group_association" "nic1" {
+  network_interface_id      = azurerm_network_interface.main.id
+  network_security_group_id = azurerm_network_security_group.nsg1.id
+}
+
+resource "azurerm_network_security_group" "nsg1" {
+    name                = "nsgwithsshopen"
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+
+    security_rule {
+        name                       = "AllowSSH"
+        priority                   = 100
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+security_rule {
+        name                       = "AllowJenkins"
+        priority                   = 120
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "8080"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+}
--- variable.tf
+++ variable.tf
@@ -1,3 +1,19 @@
 variable "acr_name" {
  default = "oguzhanhub"
 }
+
+variable "vm_name" {
+    default = "acr-vm"
+}
+
+variable "admin_username" {
+    default = "acr-vm"
+}
+
+variable "ssh_key_rg" {
+    default = "ssh"
+}
+
+variable "ssh_key_name" {
+  default = "azuresshemre"
+}
