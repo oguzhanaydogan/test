8d2a8aa4ceaa72fffe78d91ef2a96aeac9472ade
Oguzhan AYDOGAN <oguzhan.aydogan@deop.ca>
2023-06-07 18:26:55 -0400

    identity added

--- main.tf
+++ main.tf
@@ -384,3 +384,5 @@ resource "azurerm_role_assignment" "example" {
   role_definition_id = "${data.azurerm_subscription.current.id}${data.azurerm_role_definition.contributor.id}"
   principal_id       = azurerm_virtual_machine.vm1.identity[0].principal_id
 }
+
+#test
