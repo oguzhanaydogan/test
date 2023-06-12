6dacf98eff2d6512bde2ef3f781b3b00fbc3755d
Oguzhan AYDOGAN <oguzhan.aydogan@deop.ca>
2023-06-12 09:54:30 -0400

    ssl_minimal_tls_version_enforced  = TLSEnforcementDisabled

--- modules/MySql/main.tf
+++ modules/MySql/main.tf
@@ -16,8 +16,7 @@ resource "azurerm_mysql_server" "mysql" {
   infrastructure_encryption_enabled = true
   public_network_access_enabled     = false
   ssl_enforcement_enabled           = false
-  ssl_minimal_tls_version_enforced  = "TLS1_2"
-  
+  ssl_minimal_tls_version_enforced  = "TLSEnforcementDisabled"
 }
 
 resource "azurerm_mysql_database" "db" {
