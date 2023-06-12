dc1992fafdcddf91459a63bd20ce26e4e141d64a
Oguzhan AYDOGAN <oguzhan.aydogan@deop.ca>
2023-06-12 16:39:33 -0400

    mysql subnet configuration

--- variable.tf
+++ variable.tf
@@ -20,31 +20,36 @@ variable "subnets"  {
         address_prefixes = ["10.0.1.0/24"]
         delegation = true
         delegation_name = "Microsoft.Web/serverFarms"
-        
         }
     "key-vault-subnet" = {
         address_prefixes = ["10.0.2.0/24"]
         delegation = false
+        delegation_name = ""
         }
     "default_subnet" = {
         address_prefixes = ["10.0.0.0/24"]
         delegation = false
+        delegation_name = ""
         }
     "acr_subnet" = {
         address_prefixes = ["10.0.3.0/24"]
         delegation = false
+        delegation_name = ""
         }
     "appgateway_subnet" = {
         address_prefixes = ["10.0.4.0/24"]
         delegation = false
+        delegation_name = ""
         }  
     "app1endpoint_subnet" = {
         address_prefixes = ["10.0.5.0/26"]
         delegation = false
+        delegation_name = ""
         } 
     "app2endpoint_subnet" = {
         address_prefixes = ["10.0.5.64/26"]
         delegation = false
+        delegation_name = ""
         } 
     "mysql_subnet" = {
         address_prefixes = ["10.0.6.0/24"]
