eb057cd51b3018726273dd9d729f11dbbff164d6
ccseyhan <cemalcan.seyhan@deop.ca>
2023-06-16 14:59:46 -0300

    vnet peering

--- main.tf
+++ main.tf
@@ -381,7 +381,7 @@ module "mysql" {
   depends_on = [ module.private_dns_zone_mysql ]
 }
 
-module "private_dns_zone_mysql_flex" {
+module "private_dns_zone_mysql" {
   source = "./modules/privatednszonewithlink"
   name = "privatelink.mysql.database.azure.com"
   resourcegroup = module.resourcegroup.name
